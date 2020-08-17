dbac6d65ddfcbba3cb3a7ff1f9f267215de600ba
mushmomroom <crayonkidcharlieshadow@gmail.com>
2020-08-16 23:02:27 -0700

    [bug] bug fixed for backwards linking not working

---
+++ javascript/current.js
@@ -0,0 +1,7 @@
+var fs = require("fs");
+var text = fs.readFileSync("../internship.txt");
+var textByLine = text.split("\n");
+intern = $("#internship");
+for (i = 0; i < textByLine.length; i++) {
+  intern.innerHTML += "<p>" + `${i} ` + arr[i] + "</p><br>";
+}
