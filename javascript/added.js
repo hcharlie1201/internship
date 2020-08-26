c6edc743bc905fa15a8043397ba2d282b8f3cf86
mushmomroom <crayonkidcharlieshadow@gmail.com>
2020-08-26 14:08:54 -0700

    chores

--- javascript/added.js
+++ javascript/added.js
@@ -1,10 +1,7 @@
 var fs = require("fs");
 var text = fs.readFileSync("../added.txt");
-var textByLine = text.split("\n")
-added = $('#added')
-for(i = 0; i < textByLine.length; i++) {
-    added.innerHTML += "<p>" +`${i} `+ arr[i] + "</p><br>";
-}
-function mainpage() {
-    $('a').load('index.html')
+var textByLine = text.split("\n");
+added = $("#added");
+for (i = 0; i < textByLine.length; i++) {
+  added.innerHTML += "<p>" + `${i} ` + arr[i] + "</p><br>";
 }
--- javascript/current.js
+++ javascript/current.js
@@ -1,7 +1,7 @@
-var fs = require("fs");
-var text = fs.readFileSync("../internship.txt");
-var textByLine = text.split("\n");
-intern = $("#internship");
-for (i = 0; i < textByLine.length; i++) {
-  intern.innerHTML += "<p>" + `${i} ` + arr[i] + "</p><br>";
-}
+document.getElementById("inputfile").addEventListener("change", function () {
+  var fr = new FileReader();
+  fr.onload = function () {
+    document.getElementById("output").textContent = fr.result;
+  };
+  fr.readAsText(this.files[0]);
+});
